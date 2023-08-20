063c5291360b09537d42f34c554fa2d6a9ded1c1
Ryan Will Darós <ryan_will-@hotmail.com>
2023-08-19 22:07:41 -0300

    Primeiro Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,172 @@
+'use client'
+//* Essenciais
+import { useState, useEffect } from 'react'
 import Image from 'next/image'
+import { useCompletion } from 'ai/react'
+
+//* React Ace -> Textarea em código
+import AceEditor from 'react-ace'
+import 'ace-builds/src-noconflict/mode-sql'
+import 'ace-builds/src-noconflict/theme-dracula'
+import 'ace-builds/src-noconflict/ext-language_tools'
+
+//* Estilização
+import GlobalStyles from '@mui/material/GlobalStyles'
+import logoImage from '../assets/logo.svg'
+import { Trash2, Stars } from 'lucide-react'
+
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 export default function Home() {
+  const [schema, setSchema] = useState<string>('')
+  const [showResult, setShowResult] = useState<boolean>(false)
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+
+  const { completion, handleSubmit, input, handleInputChange } = useCompletion({
+    api: './api/completion',
+    body: {
+      schema,
+    },
+  })
+
+  const clearFields = () => {
+    setSchema('')
+    setShowResult(false)
+    const clearEvent = { target: { value: '' } } as React.ChangeEvent<HTMLInputElement>
+    handleInputChange(clearEvent)
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 200)
+  }, [])
+
+  useEffect(() => {
+    console.log(completion)
+    completion != '' && setShowResult(true)
+  }, [completion])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <SkeletonTheme baseColor="#202020" highlightColor="#444">
+        <div className="max-w-[430px] px-4 mx-auto pt-12 pb-4">
+          <GlobalStyles
+            styles={{
+              '*::-webkit-scrollbar': {
+                width: '.2em',
+              },
+              '*::-webkit-scrollbar-track': {
+                '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.00)',
+              },
+              '*::-webkit-scrollbar-thumb': {
+                backgroundColor: 'rgba(0,0,0,.1)',
+                outline: '1px solid slategrey',
+              },
+            }}
+          />
+          <header className="flex items-center justify-between">
+            <Image src={logoImage} alt="Logotipo ask sql" />
+            <button type="button">
+              <Trash2 onClick={clearFields} className="h-8 w-8 text-white" strokeWidth={0.8} />
+            </button>
+          </header>
+
+          <form onSubmit={handleSubmit} className="py-8 w-full flex flex-col text-foam">
+            <label className="text-lg font-light" htmlFor="schema">
+              Cole a schema da sua tabela / Banco de dados SQL aqui:
+            </label>
+            {isLoading ? (
+              <Skeleton height={150} className="my-4 h-full max-w-full" />
+            ) : (
+              <AceEditor
+                mode="sql"
+                theme="dracula"
+                fontSize={15}
+                name="schema"
+                height="150px"
+                className=" my-4 h-full max-w-full bg-blueberry-600 border border-blueberry-300 rounded-md px-4 py-3 transition-all duration-300"
+                showPrintMargin={true}
+                wrapEnabled={true}
+                showGutter={true}
+                onChange={(schema) => setSchema(schema)}
+                highlightActiveLine={false}
+                value={schema}
+                style={{
+                  fontFamily: '"Fira code", "Fira Mono", monospace',
+                }}
+                setOptions={{
+                  enableBasicAutocompletion: false,
+                  enableLiveAutocompletion: false,
+                  enableSnippets: false,
+                  showLineNumbers: true,
+                  tabSize: 2,
+                }}
+              />
+            )}
+            <label className="text-lg font-light" htmlFor="question">
+              Faça uma pergunta sobre o código:
+            </label>
+
+            {isLoading ? (
+              <Skeleton height={160} className="my-4 rounded-md px-4 h-40 max-h-40 py-5 " />
+            ) : (
+              <textarea
+                className="my-4 resize-none bg-blueberry-600 border border-blueberry-300 rounded-md px-4 h-40 max-h-40 py-5 outline-none transition-all duration-300 focus:border-lime-300"
+                name="question"
+                id="question"
+                value={input}
+                onChange={handleInputChange}
+              />
+            )}
+
+            {isLoading ? (
+              <Skeleton className="h-14" />
+            ) : (
+              <button
+                type="submit"
+                className="text-pistachio flex items-center justify-center rounded-lg border border-pistachio h-14 gap-2  hover:border-lime-200 hover:bg-blueberry-300 hover:text- transition-colors duration-500">
+                <Stars className="w-6 h-6" />
+                Perguntar a inteligência artificial
+              </button>
+            )}
+          </form>
+
+          {showResult && (
+            <>
+              <div>
+                <span className="text-lg font-light text-foam">Resposta:</span>
+                <AceEditor
+                  readOnly
+                  mode="sql"
+                  theme="dracula"
+                  name="schema"
+                  fontSize={15}
+                  height="100px"
+                  className=" my-4 h-full max-w-full font-mono bg-blueberry-600 border border-blueberry-300 rounded-md px-4 py-3 transition-all duration-300"
+                  showPrintMargin={true}
+                  wrapEnabled={true}
+                  showGutter={true}
+                  style={{
+                    fontFamily: '"Fira code", "Fira Mono", monospace',
+                  }}
+                  onChange={(schema) => setSchema(schema)}
+                  highlightActiveLine={false}
+                  value={completion}
+                  setOptions={{
+                    enableBasicAutocompletion: false,
+                    enableLiveAutocompletion: false,
+                    enableSnippets: false,
+                    showLineNumbers: true,
+                    tabSize: 2,
+                  }}
+                />
+              </div>
+            </>
+          )}
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </SkeletonTheme>
+    </>
   )
 }
